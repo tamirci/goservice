470e3cc8d354b16b93679f7ac9a85ff10f769165
tamirci <zztamirci@gmail.com>
2015-09-19 18:06:56 +0300

    add error handler

--- hiservice.go
+++ hiservice.go
@@ -14,7 +14,17 @@ func main() {
 	log.Println("80 geldi")
 }
 func hello(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
 	log.Println("hello geldi")
 	fmt.Fprintf(w, "hi!")
 	log.Println("hello gecti")
+}
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "custom 404")
+	}
 }
