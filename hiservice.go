4db79f0613345ca30bd68e2cd7f5fd1a0ef49a1e
tamirci <zztamirci@gmail.com>
2015-09-19 17:43:27 +0300

    add log

--- hiservice.go
+++ hiservice.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Println("main geldi")
 	http.HandleFunc("/hello", hello)
+	log.Println("hello geldi")
 	http.ListenAndServe(":80", nil)
+	log.Println("80 geldi")
 }
 func hello(w http.ResponseWriter, r *http.Request) {
+	log.Println("hello geldi")
 	fmt.Fprintf(w, "hi!")
+	log.Println("hello gecti")
 }
