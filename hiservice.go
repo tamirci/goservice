5b5949e209e041841ad8fa4b94e2c386ed91c3aa
tamirci <zztamirci@gmail.com>
2015-09-19 18:36:21 +0300

    delete error handler

--- hiservice.go
+++ hiservice.go
@@ -9,11 +9,6 @@ func main() {
 	log.Println("main geldi")
 	http.ListenAndServe(":80", nil)
 	log.Println("80 geldi")
-	http.HandleFunc("/hello", hello)
-	log.Println("hello geldi")
-}
-func hello(w http.ResponseWriter, r *http.Request) {
-	log.Println("hello geldi")
 
-	log.Println("hello gecti")
+	log.Println("hello geldi")
 }
